fe69a6eaaa959af5d2f0014014f02c0211cf29a0
unknown <rknx@outlook.com>
2019-03-26 16:22:37 -0400

    V3

---
+++ js/main.js
@@ -0,0 +1,69 @@
+//Strict Mode 
+(function($) {
+  "use strict";
+
+//Run on Document Ready
+$(document).ready(function(){  
+
+  //Side menu - Open
+  $('.side-menu-open').mouseenter(function(){
+    $('.side-menu').animate({'left': '0px'}, 600, 'easeOutCubic');
+  });
+
+  $('.side-menu').mouseleave(function(){
+    var sideWidth = $('.side-menu').outerWidth();
+    var sideWidthClose = '-' + sideWidth + 'px';
+    $('.side-menu').animate({'left': sideWidthClose}, 600, 'easeOutCubic');
+    preventDefault();
+  });
+
+  //Side menu - Close
+  $('#side-menu-close').click(function(){
+    var sideWidth = $('.side-menu').outerWidth();
+    var sideWidthClose = '-' + sideWidth + 'px';
+    $('.side-menu').animate({'left': sideWidthClose}, 600, 'easeOutCubic');
+    preventDefault();
+  });
+
+  //Smooth Scroll on anchor links
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 700, 'easeInOutExpo');
+        return false;
+      }
+    }
+  });
+
+});
+
+//Run on Window Load
+$(window).load(function(){
+  //Page loader
+  $('#page-loader').fadeOut(500, function(){});
+
+  //Fade In load
+  $('#content-body').addClass('fadeInUp');
+
+  //Background Height fix for vertical progress
+  setTimeout(function () {    
+      $( ".full-height" ).each(function() {
+        var $stretch = $(this);
+        $stretch.css({ height: $stretch.closest('.line').find('.content-wrap').outerHeight() });
+      });  
+    }, 200
+  );
+  
+  //Background Height fix for vertical progress on window resize
+  $(window).resize(function(){ 
+     $( ".full-height" ).each(function() {
+      var $stretch = $(this);
+      $stretch.css({ height: $stretch.closest('.line').find('.content-wrap').outerHeight() });
+    }); 
+  });
+});
+})(jQuery);
