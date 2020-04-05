a6d54c9dad88845547a3f4fe5b5bc4b65e679c27
Anuj Sharma <rknx@outlook.com>
2020-04-05 18:08:45 -0400

    v4.1.7

--- js/main.js
+++ js/main.js
@@ -1,40 +1,64 @@
-//Strict Mode 
-(function($) {
+//Strict Mode
+(function ($) {
   "use strict";
 
-//Run on Document Ready
-$(document).ready(function(){
-  //Populate values from json data
-  $.getJSON("data.json", function(data){
+  //Run on Document Ready
+  $(document).ready(function () {
+    //Populate values from json data
+    $.getJSON("data.json", function (data) {
+      $(".strName").html(
+        `${data.intro.firstname} <span>${data.intro.lastname}</span>`
+      );
+      $(".strJob").html(data.intro.profession);
 
-    $('.strName').html(`${data.intro.firstname} <span>${data.intro.lastname}</span>`);
-    $('.strJob').html(data.intro.profession);
-
-    for (const val of Object.values(data.buttons)){};
-    if (typeof tlFunc[data.intro["func"]] === "function") tlFunc[data.intro["func"]](data.intro);
-    for (const [key,val] of Object.entries(data.timeline)) if (val["display"]!==false && typeof tlFunc[val["func"]] === "function") tlFunc[val["func"]](key,val);
-    if (typeof tlFunc[data.buttons["func"]] === "function") tlFunc[data.buttons["func"]](data.buttons)
-    if (typeof tlFunc[data.quote["func"]] === "function") tlFunc[data.quote["func"]](data.quote);
-
+      for (const val of Object.values(data.buttons)) {
+      }
+      if (typeof tlFunc[data.intro["func"]] === "function")
+        tlFunc[data.intro["func"]](data.intro);
+      for (const [key, val] of Object.entries(data.timeline))
+        if (
+          val["display"] !== false &&
+          typeof tlFunc[val["func"]] === "function"
+        )
+          tlFunc[val["func"]](key, val);
+      if (typeof tlFunc[data.buttons["func"]] === "function")
+        tlFunc[data.buttons["func"]](data.buttons);
+      if (typeof tlFunc[data.quote["func"]] === "function")
+        tlFunc[data.quote["func"]](data.quote);
+    });
   });
 
-});
-  
-//Background Height fix for vertical progress on window resize
-$(window).on('resize', function(){
-  console.log(window.innerWidth);
-  if (window.innerWidth>=1280) $("#menu").prop("checked", false);
-  $(".menu-open").css({"display":(window.innerWidth>=1280)?'none':''});
-  $('.side-menu').css({"clip-path":(window.innerWidth>=1280)?'circle(150% at 0 0px)':''});
-});
+  //Run on Window Resize
+  $(window).on("resize", function () {
+    //Changes to side-menu
+    if (window.innerWidth >= 1280) $("#menu").prop("checked", false);
+    $(".menu-open").css({ display: window.innerWidth >= 1280 ? "none" : "" });
+    $(".side-menu").css({
+      "clip-path": window.innerWidth >= 1280 ? "circle(150% at 0 0px)" : "",
+    });
 
-//Run on Window Load
-$(window).on('load', function(){
-  //Page loader
-  $('#page-loader').fadeOut(500, function(){});
+    //Other to be added
+  });
 
-  if('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js');
+  //Run on Window Load
+  $(window).on("load", function () {
+    //Page loader
+    $("#page-loader").fadeOut(500, function () {});
 
-});
+    //PWA Service worker
+    if ("serviceWorker" in navigator)
+      navigator.serviceWorker.register("/sw.js");
 
+    //Menu close on click
+    $("nav").click(function () {
+      $("#menu").prop("checked", false);
+      setTimeout(function () {
+        history.replaceState(
+          {},
+          document.title,
+          window.location.href.split("#")[0]
+        );
+      }, 50);
+    });
+  });
 })(jQuery);
--- js/tlfunctions.js
+++ js/tlfunctions.js
@@ -1,60 +1,74 @@
 var tlFunc = {
-  initial: function(id, name, print) {
-    $('nav').append(`<a href='#${id}'> <svg><use xlink:href='/img/icons.svg#angle-right'></svg>${name.split(" ")[0]}</a>`);
-    $('body').append(`<section class='timeline ${print}' id='${id}'>
+  initial: function (id, name, print) {
+    $("nav").append(
+      `<a href='#${id}'> <svg><use xlink:href='/img/icons.svg#angle-right'></svg>${
+        name.split(" ")[0]
+      }</a>`
+    );
+    $("body").append(`<section class='timeline ${print}' id='${id}'>
       <div class='title'><div><div></div></div><h3 class='tl-title'>${name}</h3></div>
       </section>`);
   },
 
-  intro: function({image,firstname,lastname,profession,bio,email}) {
-    $('body').append(
+  intro: function ({ image, firstname, lastname, profession, bio, email }) {
+    $("body").append(
       `<section class='header'>
         <div class='avatar'><object data='${image}' type='image/png' alt='${firstname} ${lastname}'>${firstname} ${lastname}</object></div>
         <div class='bio'>
           <h1>${firstname} ${lastname}</h1>
           <h2>${profession}</h2>
-          <p>${Object.values(bio).join( )} </p>
+          <p>${Object.values(bio).join()} </p>
         </div>
         <h6 class='onlyPrint'>Email:${email}</h6>
       </section>`
     );
   },
 
-  buttons: function({entries}) {
-    for (const {url,icon,text} of Object.values(entries)) {
-      $('.buttons').append(
+  buttons: function ({ entries }) {
+    for (const { url, icon, text } of Object.values(entries)) {
+      $(".buttons").append(
         `<a role='button' aria-label='${text}' href='${url}' class='btn'>
           <svg><use xlink:href='/img/icons.svg#${icon}'></svg>
           <span>${text}</span>
         </a>`
       );
-    };
-    $('.side-menu').append (
+    }
+    $(".side-menu").append(
       `<div class='hosts'>
         <a href='https://www.netlify.com'><svg><use xlink:href='/img/icons.svg#netlify'></svg>Deploys by Netlify</a>
         <a href='https://www.github.com'><svg><use xlink:href='/img/icons.svg#github'></svg>Hosted on Github</a>
       </div>`
     );
   },
-  
-  education: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-    for (const {degree,date,completed,institution,subinstitution} of Object.values(entries)){
-      $( `#${key}`).append(
+
+  education: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
+    for (const {
+      degree,
+      date,
+      completed,
+      institution,
+      subinstitution,
+    } of Object.values(entries)) {
+      $(`#${key}`).append(
         `<div>
           <div><div><svg><use xlink:href='/img/icons.svg#${logo}'/></svg></div></div>
           <div>
-            <h4>${degree} <span> ${date} ${((completed == false) ?  "(expected)":"")}</span></h4>
+            <h4>${degree} <span> ${date} ${
+          completed == false ? "(expected)" : ""
+        }</span></h4>
             <h5 class='entry-description'>${institution}, ${subinstitution}</h5>
           </div>
         </div>`
       );
-    };
+    }
   },
 
-  awards: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-    for (const {award,date,institution,subinstitution} of Object.values(entries)){
+  awards: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
+    for (const { award, date, institution, subinstitution } of Object.values(
+      entries
+    )) {
       $(`#${key}`).append(
         `<div>
           <div><div><svg><use xlink:href='/img/icons.svg#${logo}'/></svg></div></div>
@@ -64,14 +78,15 @@ var tlFunc = {
           </div>
         </div>`
       );
-    };
+    }
   },
 
-  skills: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-    for (const {prettyName,list} of Object.values(entries)){
-      var htmltext="";
-      for (const {icon,text} of Object.values(list)) htmltext+=`<span><img src='${icon}' alt='' onerror='this.style.display="none"'>${text}</span>`;          
+  skills: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
+    for (const { prettyName, list } of Object.values(entries)) {
+      var htmltext = "";
+      for (const { icon, text } of Object.values(list))
+        htmltext += `<span><img src='${icon}' alt='' onerror='this.style.display="none"'>${text}</span>`;
       $(`#${key}`).append(`
         <div>
           <div><div><svg><use xlink:href='/img/icons.svg#${logo}'/></svg></div></div>
@@ -79,16 +94,23 @@ var tlFunc = {
             <h4>${prettyName}</h4>
             <div class='skills'>${htmltext}</div>
           </div>
-        </div>`
-      );
-    };
+        </div>`);
+    }
   },
 
-  research: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-    for (const {position,start,end,institution,subinstitution,projects} of Object.values(entries)){
-      var htmltext="";
-      for (const list of Object.values(projects)) htmltext+=`<p class='bull-ind'>${list}</p>`;          
+  research: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
+    for (const {
+      position,
+      start,
+      end,
+      institution,
+      subinstitution,
+      projects,
+    } of Object.values(entries)) {
+      var htmltext = "";
+      for (const list of Object.values(projects))
+        htmltext += `<p class='bull-ind'>${list}</p>`;
       $(`#${key}`).append(`
         <div>
           <div><div><svg><use xlink:href='/img/icons.svg#${logo}'/></svg></div></div>
@@ -96,72 +118,72 @@ var tlFunc = {
             <h4>${position} <span>${start} – ${end} </span></h4>
             <h5>${institution}, ${subinstitution} </h5> ${htmltext}
           </div>
-        </div>`
-      );
-    };
+        </div>`);
+    }
   },
 
-  teaching: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-    for (const {list,organization} of Object.values(entries)){
-      var htmltext="";
-      for (const {position,title,course,date} of Object.values(list)) htmltext+=`<h5>${position} ${((title!="") ? "("+title+") in":"for")} ${course}<span>${date}</span></h5>`;          
+  teaching: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
+    for (const { list, organization } of Object.values(entries)) {
+      var htmltext = "";
+      for (const { position, title, course, date } of Object.values(list))
+        htmltext += `<h5>${position} ${
+          title != "" ? "(" + title + ") in" : "for"
+        } ${course}<span>${date}</span></h5>`;
       $(`#${key}`).append(`
         <div>
           <div><div><svg><use xlink:href='/img/icons.svg#${logo}'/></svg></div></div>
           <div><h4>${organization}</h4>${htmltext}</div>
-        </div>`
-      );
-    };
+        </div>`);
+    }
   },
 
-  affiliations: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-    for (const {organization,position} of Object.values(entries)){
-      var htmltext="";
-      for (const {role,start,end} of Object.values(position)) htmltext+=`<h5>${role}<span>${start} – ${end}</span></h5>`;          
+  affiliations: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
+    for (const { organization, position } of Object.values(entries)) {
+      var htmltext = "";
+      for (const { role, start, end } of Object.values(position))
+        htmltext += `<h5>${role}<span>${start} – ${end}</span></h5>`;
       $(`#${key}`).append(`
         <div>
           <div><div><svg><use xlink:href='/img/icons.svg#${logo}'/></svg></div></div>
           <div><h4>${organization}</h4>${htmltext}</div>
-        </div>`
-      );
-    };
+        </div>`);
+    }
   },
 
-  projects: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-    for (const {title,image,text} of Object.values(entries)){
+  projects: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
+    for (const { title, image, text } of Object.values(entries)) {
       $(`#${key}`).append(`
         <div>
           <div><div><svg><use xlink:href='/img/icons.svg#${logo}'/></svg></div></div>
           <div><h4>${title}</h4><div><img src='${image}' alt='' onerror='this.style.display=none'><p>${text}</p></div></div>
-        </div>`
-      );
-    };
+        </div>`);
+    }
   },
 
-  publications: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-    for (const {prettyName,list} of Object.values(entries)){
-      var htmltext="";
-      for (const {prettyName,items} of Object.values(list)){
-        var htmltext1="";
-        for (const {url,citationhtml} of Object.values(items)) htmltext1+=`<p class='bull-ind'><a href='${url}'>${citationhtml}</a></p>`;
-        htmltext+=`<h5 class='noprint'>${prettyName}</h5>${htmltext1}`;
-      };          
+  publications: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
+    for (const { prettyName, list } of Object.values(entries)) {
+      var htmltext = "";
+      for (const { prettyName, items } of Object.values(list)) {
+        var htmltext1 = "";
+        for (const { url, citationhtml } of Object.values(items))
+          htmltext1 += `<p class='bull-ind'><a href='${url}'>${citationhtml}</a></p>`;
+        htmltext += `<h5 class='noprint'>${prettyName}</h5>${htmltext1}`;
+      }
       $(`#${key}`).append(`
         <div>
           <div><div><svg><use xlink:href='/img/icons.svg#${logo}'/></svg></div></div>
           <div><h4>${prettyName}</h4>${htmltext}</div>
-        </div>`
-      );
+        </div>`);
     }
   },
 
-  presentation: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-    for (const {conference, date, titlehtml} of Object.values(entries)){
+  presentation: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
+    for (const { conference, date, titlehtml } of Object.values(entries)) {
       $(`#${key}`).append(`
         <div>
           <div><div><svg><use xlink:href='/img/icons.svg#${logo}'/></svg></div></div>
@@ -169,19 +191,19 @@ var tlFunc = {
             <h4>${conference}<span>${date}</span></h4>
             <h5 class='entry-description'>${titlehtml}</h5>
           </div>
-        </div>`
-      );
-    };
+        </div>`);
+    }
   },
 
-  posters: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-
+  posters: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
   },
 
-  references: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-    for (const {name, email, position, institution} of Object.values(entries)){
+  references: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
+    for (const { name, email, position, institution } of Object.values(
+      entries
+    )) {
       $(`#${key}`).append(`
         <div>
           <div><div><svg><use xlink:href='/img/icons.svg#${logo}'/></svg></div></div>
@@ -189,39 +211,40 @@ var tlFunc = {
             <h4>${name}<span><small>${email}</small></span></h4>
             <h5 class='entry-description'>${position}, ${institution}</h5>
           </div>
-        </div>`
-      );
-    };
+        </div>`);
+    }
   },
 
-  contact: function(key, {prettyName,logo,noPrint,entries}) {
-    this.initial(key,prettyName,noPrint );
-    for (const [ind,{prettyName,list}] of Object.entries(entries)){
-      var htmltext="";
-      for (const {prettyName,content,icon,link} of Object.values(list)){
-        htmltext+=`
-          <h5 class='${ind}'> ${((prettyName != "") ? prettyName+":"  :"" )}
+  contact: function (key, { prettyName, logo, noPrint, entries }) {
+    this.initial(key, prettyName, noPrint);
+    for (const [ind, { prettyName, list }] of Object.entries(entries)) {
+      var htmltext = "";
+      for (const { prettyName, content, icon, link } of Object.values(list)) {
+        htmltext += `
+          <h5 class='${ind}'> ${prettyName != "" ? prettyName + ":" : ""}
             <click tabindex='0'></click><a href='${link}'>
-            ${((content != "") ? content+"&nbsp;" :"" )}
-            ${((icon != "") ? "<svg><use xlink:href='/img/icons.svg#"+icon+"'/></svg>"  : "" )}
+            ${content != "" ? content + "&nbsp;" : ""}
+            ${
+              icon != ""
+                ? "<svg><use xlink:href='/img/icons.svg#" + icon + "'/></svg>"
+                : ""
+            }
             </a>
           </h5>`;
-      };        
+      }
       $(`#${key}`).append(`
         <div>
           <div><div><svg><use xlink:href='/img/icons.svg#${logo}'/></svg></div></div>
           <div><h4>${prettyName}</h4>${htmltext}</div></div>
-        </div>`
-      );
-    };
+        </div>`);
+    }
   },
 
-  quote: function ({noPrint,logo, text, speaker}) {
-    $('body').append(`
+  quote: function ({ noPrint, logo, text, speaker }) {
+    $("body").append(`
       <div id='quote' class='${noPrint}'>
         <svg><use xlink:href='/img/icons.svg#${logo}'></use></svg>
         <p>${text}</p><span>${speaker}</span>
-      </div>`
-    );
-  }
-}
+      </div>`);
+  },
+};
